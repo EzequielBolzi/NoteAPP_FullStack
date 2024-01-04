5aadb7af073babe49cecc75f94afa2b1e1292833
Ezequiel Bolzi <ezequielbolzi@hotmail.com>
2024-01-04 12:23:54 -0300

    Update docker

--- Backend/rest/src/main/java/com/ezequielbolzi/rest/RestApplication.java
+++ Backend/rest/src/main/java/com/ezequielbolzi/rest/RestApplication.java
@@ -3,7 +3,6 @@ package com.ezequielbolzi.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class RestApplication {
 
 	public static void main(String[] args) {
